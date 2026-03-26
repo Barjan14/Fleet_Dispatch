12a3979e41daa45ccfac4bc2243427e8affbc0df
Bryle Jan Nacalaban <bryle.jan14@gmail.com>
2026-03-26 17:43:11 +0800

    in the begingging2

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('login/', views.user_login_view, name='login'),
     path('admin-login/', views.admin_login_view, name='admin_login'),
     path('dashboard/', views.dashboard, name='dashboard'),
+    path('admin-dashboard/', views.admin_dashboard, name='admin_dashboard'),
     path('logout/', views.logout_view, name='logout'),
 ]
